22d7806bc4aa489cf37a4b645ba0d84feaaaaabd
vs7141-hub <vs7141@srmist.edu.in>
2026-04-13 23:16:13 +0530

    Uc2

---
+++ Helloapp/src/main/Uc2.java
@@ -0,0 +1,11 @@
+public class Uc2 {
+     public static void main(String[] args) {
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
+    }
+}
+
